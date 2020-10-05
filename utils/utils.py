c3472b5c751dde13192cdf41c9a7e374e93357f1
Krishna Prasad <krishna.kp07@gmail.com>
2020-10-05 16:45:19 +0530

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,23 +1,14 @@
-import tensorflow as tf
-import timeit
-
-def normalize(x, eps = 1e-8):
-    mean = tf.math.reduce_mean(x)
-    std = tf.math.reduce_std(x)
-    return (x - mean)/(std + eps)
+from functools import wraps
+from time import time
 
 
-# A decorator to measure execution times of functions
-# add logging functionality
 def timer(func):
-    def wrapper(*args, **kwargs):
-        start_time = timeit.default_timer()
-        func_val = func(*args, **kwargs)
-        elapsed = timeit.default_timer() - start_time
-        print('Function "{name}" took {time} seconds to complete.'.format(name=func.__name__, time=elapsed))
-        return func_val
-    return wrapper
-
-
-if __name__ == "__main__":
-    pass
+    @wraps(func)
+    def _time_it(*args, **kwargs):
+        start = int(round(time() * 1000))
+        try:
+            return func(*args, **kwargs)
+        finally:
+            end_ = int(round(time() * 1000)) - start
+            print(f"Total execution time: {end_ if end_ > 0 else 0} ms")
+    return _time_it
